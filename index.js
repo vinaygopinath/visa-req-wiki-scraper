65e6fe8d2a95d4ba5d6f2bbb46d45d4dea63d5ca
Vinay Gopinath <vinayg18@gmail.com>
2016-06-25 18:31:18 +0200

    [Index.js] Scrape wiki page, save to file and wait 5s before next

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 const scrapeCountryData = require('./tasks/scrape-country-data');
-const fs = require('fs');
+const writeToFile = require('./tasks/write-to-file');
 const Promise = require('bluebird');
+const readFile = Promise.promisify(require('fs').readFile);
 
-const countries = JSON.parse(fs.readFileSync('./input/countries.json', 'utf8'));
-
-if (!countries || !countries.length) {
-  console.error('Could not load list of countries. Check if countries.json exists');
-  return;
-}
-console.log('countries loaded, proceeding with scraping');
-Promise.mapSeries(countries, function(country) {
-  return scrapeCountryData(country)
-  .then(function(data) {
-    return Promise.delay(5000, data);
+readFile('./input/countries.json', 'utf8')
+  .then(function(countriesStr) {
+    return Promise.resolve(JSON.parse(countriesStr));
+  }, function(err) {
+    console.error('Could not load list of countries. Check if countries.json exists');
+    console.dir(err);
+  })
+  .then(function(countries) {
+    // console.log('Number of countries = ' + countries.length);
+    return Promise.mapSeries(countries, function(country) {
+      // console.log('Processing ' + country.name);
+      return scrapeCountryData(country.demonym)
+        .then(function(scrapeResponse) {
+          console.log('\n*****\n' + country.name + ' stats:');
+          console.log('Visa not required: %d', scrapeResponse['not-required'].length);
+          console.log('Visa required: %d', scrapeResponse['required'].length);
+          console.log('Visa on arrival: %d', scrapeResponse['on-arrival'].length);
+          console.log('Visa status unknown: %d', scrapeResponse['unknown'].length);
+          console.log('Total countries: %d\n*****\n', scrapeResponse['not-required'].length
+            + scrapeResponse['required'].length + scrapeResponse['on-arrival'].length
+            + scrapeResponse['unknown'].length);
+          return writeToFile(country.name, scrapeResponse);
+        })
+        .then(function() {
+          //Add a mandatory delay of 5s between page loads
+          return Promise.delay(5000);
+        })
+        .catch(function(err) {
+          console.log('Error in promise chain: ', err);
+        });
+    });
+  }, function(err) {
+    console.error('Error parsing string from countries.json. Please check the file contents');
+    console.dir(err);
+  })
+  .then(function() {
+    console.log('Processed all Wikipedia pages');
   });
-})
-.then(function(allCountiesData) {
-  allCountiesData = allCountiesData || [];
-  allCountiesData.forEach(function(countryData) {
-    console.log('countryData = ',countryData);
-  });
-  //TODO Write data to corresponding country file
-});
